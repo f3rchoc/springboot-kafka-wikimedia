f9b5f800f57b2ec4287537602b7c8d3c3ee297a5
Fernando <wilmercaiza@gmail.com>
2023-10-24 12:24:09 -0500

    Create project

---
+++ kafka-producer-wikimedia/src/test/java/ec/ferchoc/springboot_kafka/SpringBootProducerApplicationTest.java
@@ -0,0 +1,13 @@
+package ec.ferchoc.springboot_kafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootProducerApplicationTest {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ src/test/java/ec/ferchoc/springboot_kafka/SpringbootKafkaWikimediaApplicationTests.java
@@ -0,0 +1,13 @@
+package ec.ferchoc.springboot_kafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootKafkaWikimediaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
